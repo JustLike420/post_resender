e792405470e13bb33fa8188bc74a1962fa858bf2
Vladimir <justlikevova@gmail.com>
2022-10-22 01:04:33 +0300

    face book sender

--- facebook_sender.py
+++ facebook_sender.py
@@ -1,14 +1,49 @@
 import facebook
 
+import json
+import time
+from vk_parser import VkParser
 
-group = '3071372716522115'
-token=''
-msg = 'test text'
-image_url = 'https://sun9-6.userapi.com/impg/mixite2huffOMRRfpWsAlEZbfwxtVydii_aJzA/MCPTBqakNXA.jpg?size=827x990&quality=96&sign=c27f062a8a33429b4804a5e541a6c9c1&c_uniq_tag=oNnVXAI6J4z-kqzPxMp4G1D_DG_vcVOppqqrVv1pwVU&type=album'
-graph = facebook.GraphAPI(access_token=token)
-video_url = 'https://vk.com/video-211099549_456239031'
+group = ''
+token = ''
 
-x = graph.put_object(group, 'feed', message=msg)
-x = graph.put_object(group, 'photos',  message=msg, url=image_url)
-x = graph.put_object(group, 'feed', message=msg, link=video_url)
-# print(x)
+
+def send_post(text, photos, videos):
+    graph = facebook.GraphAPI(access_token=token)
+    if len(photos) == 0 and len(videos) == 0:
+        x = graph.put_object(group, 'feed', message=text)
+    elif len(photos) == 1:
+        x = graph.put_object(group, 'photos', message=text, url=photos[0])
+    elif len(photos) > 1:
+        x = graph.put_object(group, 'photos', message=text, url=photos[0])
+    elif 1 < len(photos) <= 10:
+        pass
+    elif len(videos) == 1:
+        x = graph.put_object(group, 'feed', message=text, link=videos[0])
+    print('sent post')
+
+
+def main():
+    with open("publics1.json", "r") as f:
+        groups = json.load(f)
+    for group in groups:
+
+        vk = VkParser(vk_group=group["vk_id"])
+        posts = vk.get_posts()
+        new_last_id = posts[0]["id"]
+        if new_last_id > group["last_post_id"]:
+            post = posts[0]
+            vk.parse_post(post)
+            print(vk.photos)
+            send_post(vk.text, vk.photos, vk.videos)
+            group["last_post_id"] = new_last_id
+            with open('publics1.json', 'w') as outfile:
+                json.dump(groups, outfile)
+
+
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        time.sleep(60 * 5)
